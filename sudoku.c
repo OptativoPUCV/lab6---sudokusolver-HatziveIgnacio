ed13f262eeef5abf4503085418b337f8f7038fb6
Replit user <>
2023-05-05 03:54:28 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -96,17 +96,15 @@ int is_valid(Node* n){
 List* get_adj_nodes(Node* n){
    List* list=createList();
 
-   // Recorrer todas las casillas del nodo
-   for(int i=0; i<9; i++){
+   // Recorrer casillas 
+  for(int i=0; i<9; i++){
       for(int j=0; j<9; j++){
          if(n->sudo[i][j]==0){
-
-            // Aplicar las acciones al estado actual
             for(int k=1; k<=9; k++){
                Node* new_node = copy(n);
                new_node->sudo[i][j] = k;
 
-               // Verificar si el nuevo estado es válido
+               // estado es válido
                if(is_valid(new_node)){
                   pushBack(list, new_node);
                }
