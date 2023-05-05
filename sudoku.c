1fcb1a090dbdd49fbc1440307dba2fdf2e0b2769
Replit user <>
2023-05-05 03:38:23 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -62,7 +62,7 @@ List* get_adj_nodes(Node* n){
           {
             new_node = copy(n);
             new_node->sudo[i][j] = k;
-            pushFront(list, new_node);
+            pushCurrent(list, new_node);
           }
         }
         break;
